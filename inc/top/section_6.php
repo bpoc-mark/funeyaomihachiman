0eb5bde64315c1a83fbdb8bf2d0df6f93be8f493
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-08-27 14:15:28 +0800

    replace map

--- inc/top/section_6.php
+++ inc/top/section_6.php
@@ -1,9 +1,7 @@
 <section class="sect_6" id="sect_6">
     <div class="mapouter">
         <div class="gmap_canvas">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.473385433963!2d135.95993911524076!3d35.019885480353764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001723fee6f47df%3A0x2309dfdaf40065f6!2z5Yi66Lqr44Go6a2a6aOvIEZVTkVZQQ!5e0!3m2!1sja!2sph!4v1624005123492!5m2!1sja!2sph"
-                width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2274.4809385932913!2d136.10033727159643!3d35.12198247438268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60017bceb38834c7%3A0x9b882876e55062f4!2sFUNEYA%20Omihachimanten!5e0!3m2!1sen!2sph!4v1630044689409!5m2!1sen!2sph" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     <div>
 </section>
