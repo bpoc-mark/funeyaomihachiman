e3ae47f332c670d88adbd9d7acb980e556722105
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-08-27 16:12:49 +0800

    first commit

--- inc/top/section_10.php
+++ inc/top/section_10.php
@@ -5,23 +5,46 @@
             仕切りをとれば最大34名様までの団体様でのご利用もOKです。
         </p>
         <div class="l-wrap">
+            <!-- PC -->
             <div class="c-card_boxes_pc">
                 <div class="box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_1.jpeg" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_1.png" alt="">
+                    <p class="title">カウンター 全4席</p>
+                    <p class="desc">目の前で調理が見えるカウンター席。落ち着いた雰囲気でゆったりお食事をお楽しみ頂けます。</p>
+                </div>
+
+                <div class="box">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_2.png" alt="">
+                    <p class="title">テーブル 全8卓</p>
+                    <p class="desc">ご宴会はもちろん、ちょい飲み、接待など様々な食事シーンでご利用頂けます。</p>
+                </div>
+
+                <div class="box">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_3.png" alt="">
+                    <p class="title">漁港の小屋をイメージし細部 
+                        にまでこだわったインテリア</p>
+                    <p class="desc">少人数でご利用の際でも楽しんで頂けます。</p>
+                </div>
+            </div>
+
+            <!-- SP -->
+            <div class="c-card_boxes_sp">
+                <div class="box">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_1_sp.png" alt="">
                     <p class="title">カウンター 全8席</p>
                     <p class="desc">目の前で調理が見えるカウンター席。落ち着いた雰囲気でゆったりお食事をお楽しみ頂けます。</p>
                 </div>
 
                 <div class="box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_2.jpeg" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_2_sp.png" alt="">
                     <p class="title">テーブル 全6卓</p>
-                    <p class="desc">目の前で調理が見えるカウンター席。落ち着いた雰囲気でゆったりお食事をお楽しみ頂けます。</p>
+                    <p class="desc">全席視線が合わないように間仕切りがございますので、ゆっくりお食事していただけます。</p>
                 </div>
 
                 <div class="box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_3.jpeg" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_10/sect_10_img_3_sp.png" alt="">
                     <p class="title">掘りごたつ 全6卓</p>
-                    <p class="desc">目の前で調理が見えるカウンター席。落ち着いた雰囲気でゆったりお食事をお楽しみ頂けます。</p>
+                    <p class="desc">脚を伸ばして楽にお座りいただけます。ご宴会はもちろん、ちょい飲み、接待など様々な食事シーンでご利用頂けます。</p>
                 </div>
             </div>
         </div>
